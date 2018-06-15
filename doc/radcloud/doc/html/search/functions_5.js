0342f1ee3fc7225615c6c8193a07b690e86f2688
Peter Pollak <peter.pollak@parkbob.com>
2018-06-15 21:17:08 +0200

    update radcloud documentation

--- doc/radcloud/doc/html/search/functions_5.js
+++ doc/radcloud/doc/html/search/functions_5.js
@@ -2,7 +2,10 @@ var searchData=
 [
   ['onactivityresult',['onActivityResult',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_main_activity.html#ad31d9aa9ec5da472c81e8b9d4e653bba',1,'io::github::syzygy2048::radcloud::MainActivity']]],
   ['oncreate',['onCreate',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_loading_screen_activity.html#a54bc9c6e108e50ba62aff7cc64e812b5',1,'io.github.syzygy2048.radcloud.LoadingScreenActivity.onCreate()'],['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_main_activity.html#a3f1dec2ae5371ebbd577d105c8834d74',1,'io.github.syzygy2048.radcloud.MainActivity.onCreate()'],['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_rad_cloud_activity.html#a3f26b57f925b135a3242d688b93c9711',1,'io.github.syzygy2048.radcloud.RadCloudActivity.onCreate()']]],
+  ['onitemselected',['onItemSelected',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_main_activity.html#a1c009aced689f6a8c7e292db427d93b1',1,'io::github::syzygy2048::radcloud::MainActivity']]],
+  ['onnothingselected',['onNothingSelected',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_main_activity.html#ac36d766cbb4525f9c909e43376dec28e',1,'io::github::syzygy2048::radcloud::MainActivity']]],
   ['onrequestpermissionsresult',['onRequestPermissionsResult',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_main_activity.html#aadc64331c63553c85bfc45ce9cee66b2',1,'io::github::syzygy2048::radcloud::MainActivity']]],
+  ['onresume',['onResume',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_main_activity.html#a9ede421e0220985892cd18f87d0b8bba',1,'io::github::syzygy2048::radcloud::MainActivity']]],
   ['outsidethering',['outsideTheRing',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_document_manager.html#a418e7db1e3a441cbcf4b5bd2b90b31fd',1,'io::github::syzygy2048::radcloud::DocumentManager']]],
   ['overlaps',['overlaps',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_document_manager.html#acf53af046d9fbe0df0dd1c062a2c2398',1,'io::github::syzygy2048::radcloud::DocumentManager']]]
 ];
--- doc/radcloud/doc/html/search/functions_6.js
+++ doc/radcloud/doc/html/search/functions_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['process',['process',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_document_manager.html#a00bcd6e86e3cd7b82122d06c09135963',1,'io::github::syzygy2048::radcloud::DocumentManager']]],
+  ['process',['process',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_document_manager.html#a93d7c5cd7651bbe8b0769c9d071d4488',1,'io::github::syzygy2048::radcloud::DocumentManager']]],
   ['processinputfile',['processInputFile',['../classio_1_1github_1_1syzygy2048_1_1radcloud_1_1_document_manager.html#a9fab6a75480c9f0aa1fa2085359dd8fa',1,'io::github::syzygy2048::radcloud::DocumentManager']]]
 ];
